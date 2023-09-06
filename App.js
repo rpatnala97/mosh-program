7123aefd5d014e946bdc3cac4ee9144bfdcb2bac
Rahul P <109705171+rpatnala97@users.noreply.github.com>
2023-09-06 15:35:07 -0500

    Add files via upload
    Project Vidly which will display all the movieslist and genres list which we can filter and sort the movies and genres. When there are no movies in the database it will display there are no movies in the list.

---
+++ App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import Movies from './components/movies';
+import './App.css';
+
+function App() {
+  return (
+    <main className='container'>
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
